269b477611a1db0c06c1d49b57cce0982bfd7c5d
杨再润 <yangzair@outlook.com>
2023-03-06 16:15:56 +0800

    1

--- t.py
+++ t.py
@@ -13,7 +13,7 @@ import numpy as np
 #将一张图片大小转换为1080*960，然后保存在output文件夹中
 def resize_img(img_path):
     img = Image.open(img_path)
-    img = img.resize((1080,960),Image.ANTIALIAS)
+    img = img.resize((960,1024),Image.ANTIALIAS)
     img.save(img_path)
 
 #将before文件夹中的图片使用resize_img函数转换为1080*960，然后保存在output文件夹中
@@ -21,7 +21,7 @@ def resize_img_folder():
     img_path = os.path.join(os.getcwd(),'before')
     img_list = os.listdir(img_path)
     for img in img_list:
-        img_path = os.path.join(os.getcwd(),'before',img)
+        img_path = os.path.join(os.getcwd(),'output',img)
         resize_img(img_path)
 
 resize_img_folder()
--- 变换图像大小.py
+++ 变换图像大小.py
@@ -4,3 +4,27 @@
 # @Time      :2023/3/6 10:15
 # @Author    :Tim-Saijun
 # @Site      : http://zairun.eu.org
+
+import os
+from PIL import Image
+import numpy as np
+
+#将一张图片大小转换为1080*960，然后保存在output文件夹中
+def resize_img(img_path,img_path2):
+    img = Image.open(img_path)
+    img = img.resize((960,1024),Image.ANTIALIAS)
+    img.save(img_path2)
+    print(img_path)
+
+#将before文件夹中的图片使用resize_img函数转换为1080*960，然后保存在output文件夹中
+def resize_img_folder():
+    img_path = os.path.join(os.getcwd(),'before')
+    img_list = os.listdir(img_path)
+    for img in img_list:
+        img_path = os.path.join(os.getcwd(),'before',img)
+        img_path2 = os.path.join(os.getcwd(), 'output', img)
+        resize_img(img_path,img_path2)
+
+if __name__ == '__main__':
+    resize_img_folder()
+
